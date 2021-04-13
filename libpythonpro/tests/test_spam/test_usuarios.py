2e9dfdac7ac2aa39aade77cb895c8ce18b79ebfc
Slimelvl1 <pedroh.af@hotmail.com>
2021-04-13 18:34:30 -0300

    Alterado gestor de dependências para pipenv
    close #18

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,9 +1,11 @@
 from libpythonpro.spam.modelos import Usuario
 
+
 def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Pedro', email='necromancerdoidao@outlook.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
+
 
 def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Pedro', email='necromancerdoidao@outlook.com'),
